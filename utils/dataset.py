f9d88df6881e30e1d795a42d639b9c7311e60e7d
Rakshit Shetty <callrmshetty@gmail.com>
2020-07-22 13:54:11 +0530

    modified to handle custom datasets

--- utils/dataset.py
+++ utils/dataset.py
@@ -9,6 +9,8 @@ from torchvision import transforms
 from skimage import io, transform
 from PIL import Image
 
+processed_folder = 'processed'
+
 trans = transforms.Compose([
 	transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -21,14 +23,20 @@ class PreProcessDataset(Dataset):
 		self.root = os.path.expanduser(root)
 		self.train = train
 		if self.train:
-			self.train_dir = os.path.join(self.root + '/train')
-			self._resize(self.train_dir)
+			self.train_source = os.path.join(self.root, 'train')
+			self.train_dir = os.path.join(self.root, processed_folder, 'train')
+			if not self.train_dir.exists():
+				self.train_dir.mkdir()
+				self._resize(self.train_source, self.train_dir)
 			train_images = glob.glob((self.train_dir + '/*'))
 			np.random.shuffle(train_images)
 			self.train_image = list(train_images)
 		else:
-			self.test_dir = os.path.join(self.root + '/test')
-			self._resize(self.test_dir)
+			self.test_source = os.path.join(self.root, 'test')
+			self.test_dir = os.path.join(self.root, processed_folder, 'test')
+			if not self.test_dir.exists():
+				self.test_dir.mkdir(exist_ok=True)
+				self._resize(self.test_source, self.test_dir)
 			test_images = glob.glob((self.test_dir + '/*'))
 			np.random.shuffle(test_images)
 			self.test_image = list(test_images)
@@ -36,7 +44,7 @@ class PreProcessDataset(Dataset):
 		self.transforms = transform
 
 	@staticmethod
-	def _resize(source_dir):
+	def _resize(source_dir, target_dir):
 		print(f'Start Resizing {source_dir} ')
 		for i in tqdm(os.listdir(source_dir)):
 			filename = os.path.basename(i)
@@ -53,7 +61,7 @@ class PreProcessDataset(Dataset):
 						W = 512
 						H = int(ratio*W)
 					image = transform.resize(image, (H, W), mode='reflect', anti_aliasing=True)
-					io.imsave(os.path.join(source_dir, filename), image)
+					io.imsave(os.path.join(target_dir, filename), image)
 			except:
 				continue
 
@@ -65,11 +73,11 @@ class PreProcessDataset(Dataset):
 
 	def __getitem__(self, index):
 		if self.train:
-			image = self.train_image[index]
+			image, target = self.train_image[index], 1
 			image = Image.open(image)
 			#style_image = Image.open(style_image)
 		else:
-			image = self.test_image[index]
+			image, target = self.test_image[index], 1
 			image = Image.open(image)
 		
 		if self.transforms:
--- utils/make_frame.py
+++ utils/make_frame.py
@@ -5,9 +5,6 @@ import os
 save_dir = Path('../dataset/')
 save_dir.mkdir(exist_ok=True)
 
-img_dir = save_dir.joinpath('images/')
-img_dir.mkdir(exist_ok=True)
-
 cap = cv2.VideoCapture("path_to_video")
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
@@ -22,11 +19,11 @@ while (cap.isOpened()):
         cap.release()
         break
     if i < train:
-    	train_dir = img_dir.joinpath('train')
+    	train_dir = save_dir.joinpath('train')
     	train_dir.mkdir(exist_ok=True)
     	cv2.imwrite(str(train_dir.joinpath('{:05}.png'.format(i))), frame)
     else:
-    	test_dir = img_dir.joinpath('test')
+    	test_dir = save_dir.joinpath('test')
     	test_dir.mkdir(exist_ok=True)
     	cv2.imwrite(str(test_dir.joinpath('test/{:05}.png'.format(i))), frame)
     if i%1000 == 0:
